814707fda27eb6310625afe7ed5f9368848072fc
imjae <code@kakao.com>
2021-01-13 21:18:14 +0900

    add usePreventLeave Hooks

---
+++ src/UsePreventLeave.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./styles.css";
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+
+  return { enablePrevent, disablePrevent };
+};
+
+const App = () => {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>protected!</button>
+      <button onClick={disablePrevent}>unprotected!</button>
+    </div>
+  );
+};
+
+export default App;
